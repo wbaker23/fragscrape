62c9956964b926d4ab32919b56cf4d20a3c043d8
wbaker23 <bakerwill764@gmail.com>
2023-12-04 16:46:23 -0600

    Combine pages into single dict, run import

--- import_awards.py
+++ import_awards.py
@@ -13,17 +13,16 @@ from tqdm import tqdm
 DRIVER = undetected_chromedriver.Chrome()
 DRIVER.implicitly_wait(10)
 
-MENS_BEST = {
+PAGES = {
     "mens_all_time_2017": "https://www.fragrantica.com/awards/category/best-fragrance-of-all-times-for-men",
     "mens_all_time_2018": "https://www.fragrantica.com/awards2018/category/best-fragrance-for-men",
     "mens_all_time_2019": "https://www.fragrantica.com/awards2019/category/Best-Men-s-Perfume-of-All-Time",
     "mens_all_time_2020": "https://www.fragrantica.com/awards2020/category/Best-Men-s-Perfume-of-All-Time",
     "mens_all_time_2021": "https://www.fragrantica.com/awards2021/category/Best-Men-s-Perfume-of-All-Time",
     "mens_all_time_2022": "https://www.fragrantica.com/awards2022/category/Best-Men-s-Perfume-of-All-Time",
-}
-
-MENS_YEARLY = {
-    "mens_yearly_2022": "https://www.fragrantica.com/awards2022/category/Best-Perfume-for-Men-2022"
+    "mens_yearly_2022": "https://www.fragrantica.com/awards2022/category/Best-Perfume-for-Men-2022",
+    "vanilla_2021": "https://www.fragrantica.com/awards2021/category/Best-Vanilla-Perfume",
+    "hot_days_2017": "https://www.fragrantica.com/awards/category/best-fragrance-for-hot-days",
 }
 
 
@@ -181,7 +180,7 @@ def graph_years(frag_dict, ranking_func=bayesian_rating):
 
 
 if __name__ == "__main__":
-    for name, link in MENS_YEARLY.items():
+    for name, link in PAGES.items():
         print(f"Processing {link}")
         filename = f"{name}.csv"
         pd.DataFrame(get_awarded_fragrances(link)).to_csv(filename, index=False)
