dbb1488ee7dce70178584d87030b0c33e15b4ca5
wbaker23 <bakerwill764@gmail.com>
2023-12-04 18:00:58 -0600

    Import more

--- import_awards.py
+++ import_awards.py
@@ -20,9 +20,12 @@ PAGES = {
     "mens_all_time_2020": "https://www.fragrantica.com/awards2020/category/Best-Men-s-Perfume-of-All-Time",
     "mens_all_time_2021": "https://www.fragrantica.com/awards2021/category/Best-Men-s-Perfume-of-All-Time",
     "mens_all_time_2022": "https://www.fragrantica.com/awards2022/category/Best-Men-s-Perfume-of-All-Time",
+    "mens_all_time_2023": "https://www.fragrantica.com/awards2023/category/Best-Men-s-Perfume-of-All-Time",
     "mens_yearly_2022": "https://www.fragrantica.com/awards2022/category/Best-Perfume-for-Men-2022",
+    "mens_yearly_2023": "https://www.fragrantica.com/awards2023/category/Best-Perfume-for-Men-2023",
     "vanilla_2021": "https://www.fragrantica.com/awards2021/category/Best-Vanilla-Perfume",
     "hot_days_2017": "https://www.fragrantica.com/awards/category/best-fragrance-for-hot-days",
+    "best_marine_2023": "https://www.fragrantica.com/awards2023/category/Best-Marine-Perfume",
 }
 
 
