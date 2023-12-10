7a00038465965d518d6c5e650baf69df5c5b7a9a
Will Baker <wbaker@Wills-MBP.attlocal.net>
2023-12-09 22:39:26 -0600

    Use argparse in process_awards, process mens all time combined

--- code/process_awards.py
+++ code/process_awards.py
@@ -1,39 +1,46 @@
+import argparse
+import re
+
 import numpy as np
 import pandas as pd
 from config import *
 from sklearn.preprocessing import MinMaxScaler
 
+parser = argparse.ArgumentParser(
+    description="Enrich the data from a Fragrantica awards page."
+)
+parser.add_argument("filename", help="The path of the file to process.")
+parser.add_argument("output", help="The path of the file to write.")
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    for name, _ in PAGES_TO_IMPORT.items():
-        filename = f"{DATA_PATH}/{RAW_FOLDER}/{name}.csv"
+    # Load data
+    df = pd.read_csv(args.filename)
+    print(f"{df.shape[0]} fragrances loaded from {args.filename}")
 
-        # Load data
-        df = pd.read_csv(filename)
-        print(f"{df.shape[0]} fragrances loaded from {filename}")
+    # Filter out fragrances with either zero upvotes or zero downvotes
+    df = df[(df["upvotes"] != 0) & (df["downvotes"] != 0)]
 
-        # Filter out fragrances with either zero upvotes or zero downvotes
-        df = df[(df["upvotes"] != 0) & (df["downvotes"] != 0)]
+    # Get rid of bottom 10% of fragrances.
+    # These are skewed negative due to reinforcement of negative votes
+    # by being visible at the bottom of the list.
+    df = df[df["order"] <= df["order"].quantile(0.9)]
 
-        # Get rid of bottom 10% of fragrances.
-        # These are skewed negative due to reinforcement of negative votes
-        # by being visible at the bottom of the list.
-        df = df[df["order"] <= df["order"].quantile(0.9)]
+    # Add feature columns
+    df["total_votes"] = df["upvotes"] + df["downvotes"]
+    df["vote_diff"] = df["upvotes"] - df["downvotes"]
+    df["vote_diff_normalized"] = MinMaxScaler().fit_transform(
+        np.array(df["vote_diff"]).reshape(-1, 1)
+    )
+    df["ratio"] = df["upvotes"] / df["total_votes"]
+    df["updated_order"] = df["order"].rank()
+    df["inverse_order"] = df.shape[0] - df["order"].rank() + 1
+    df["bayes"] = (df["upvotes"] + df["upvotes"].median()) / (
+        df["upvotes"]
+        + df["upvotes"].median()
+        + df["downvotes"]
+        + df["downvotes"].median()
+    )
 
-        # Add feature columns
-        df["total_votes"] = df["upvotes"] + df["downvotes"]
-        df["vote_diff"] = df["upvotes"] - df["downvotes"]
-        df["vote_diff_normalized"] = MinMaxScaler().fit_transform(
-            np.array(df["vote_diff"]).reshape(-1, 1)
-        )
-        df["ratio"] = df["upvotes"] / df["total_votes"]
-        df["updated_order"] = df["order"].rank()
-        df["inverse_order"] = df.shape[0] - df["order"].rank() + 1
-        df["bayes"] = (df["upvotes"] + df["upvotes"].median()) / (
-            df["upvotes"]
-            + df["upvotes"].median()
-            + df["downvotes"]
-            + df["downvotes"].median()
-        )
-
-        # Write output
-        df.to_csv(f"{DATA_PATH}/{PROCESSED_FOLDER}/{name}.csv", index=False)
+    # Write output
+    df.to_csv(args.output, index=False)
