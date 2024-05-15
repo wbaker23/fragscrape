2cdd967c7533452cd2f5b3fa8f82e36336de3552
Will Baker <wbaker@Wills-MBP.attlocal.net>
2024-05-14 21:18:06 -0500

    use note groups in graph

--- src/fragscrape/parfumo/create_graph.py
+++ src/fragscrape/parfumo/create_graph.py
@@ -85,20 +85,20 @@ def generate_edges_df(nodes_df):
     )
     notes_cosine_array = cosine_similarity(notes_pivot)
 
-    # note_groups_exp_df = nodes_df.explode("note_groups")
-    # note_groups_exp_df["group_name"] = note_groups_exp_df["note_groups"].apply(
-    #     lambda x: x["group_name"]
-    # )
-    # note_groups_exp_df["group_count"] = note_groups_exp_df["note_groups"].apply(
-    #     lambda x: x["group_count"]
-    # )
-    # note_groups_pivot = (
-    #     note_groups_exp_df[["name", "group_name", "group_count"]]
-    #     .pivot(index="name", columns="group_name", values="group_count")
-    #     .fillna(0)
-    #     .astype("int32")
-    # )
-    # note_groups_cosine_array = cosine_similarity(note_groups_pivot)
+    note_groups_exp_df = nodes_df.explode("note_groups")
+    note_groups_exp_df["group_name"] = note_groups_exp_df["note_groups"].apply(
+        lambda x: x["group_name"]
+    )
+    note_groups_exp_df["group_count"] = note_groups_exp_df["note_groups"].apply(
+        lambda x: x["group_count"]
+    )
+    note_groups_pivot = (
+        note_groups_exp_df[["name", "group_name", "group_count"]]
+        .pivot(index="name", columns="group_name", values="group_count")
+        .fillna(0)
+        .astype("int32")
+    )
+    note_groups_cosine_array = cosine_similarity(note_groups_pivot)
 
     votes_pivot = (
         type_pivot.join(occasion_pivot).join(season_pivot).join(audience_pivot)
@@ -124,7 +124,7 @@ def generate_edges_df(nodes_df):
                 "season_similarity": season_cosine_array[i][j],
                 "audience_similarity": audience_cosine_array[i][j],
                 "notes_similarity": notes_cosine_array[i][j],
-                # "note_groups_similarity": note_groups_cosine_array[i][j],
+                "note_groups_similarity": note_groups_cosine_array[i][j],
                 "votes_similarity": votes_pivot_cosine_array[i][j],
             }
             for i in range(0, len(type_cosine_array))
@@ -195,10 +195,10 @@ def create_graph(ctx, color_groups, threshold):
         "season_similarity",
         "audience_similarity",
         "notes_similarity",
-        # "note_groups_similarity",
+        "note_groups_similarity",
         "votes_similarity",
     ]
-    component_weights = [0, 0, 0, 0, 1, 9]
+    component_weights = [0, 0, 0, 0, 0, 1, 9]
     # edges_df[component_columns] = pd.DataFrame(
     #     StandardScaler().fit_transform(edges_df[component_columns].values)
     # )
