5ead210b54464105f10642d49a62031b883425ea
Will Baker <wbaker@Wills-MBP.attlocal.net>
2024-05-17 22:18:32 -0500

    Use sqlite for parfumo enrich

--- src/fragscrape/parfumo/enrich.py
+++ src/fragscrape/parfumo/enrich.py
@@ -5,6 +5,7 @@ import click
 from selenium.webdriver.common.by import By
 from tqdm import tqdm
 
+from fragscrape.parfumo import database
 from fragscrape.parfumo.driver import start_driver
 
 
@@ -21,17 +22,45 @@ def _get_chart_data(driver, i, j):
         return type
 
 
+@database.db_connection
+def _get_collection(cursor):
+    return [dict(o) for o in cursor.execute("SELECT * FROM collection").fetchall()]
+
+
+@database.db_connection
+def _update_collection_row(cursor, data):
+    cursor.execute(
+        """
+        UPDATE collection SET
+        name = :name,
+        brand = :brand,
+        image_src = :image_src
+        WHERE link = :link
+        """,
+        data,
+    )
+
+
+@database.db_connection
+def _update_votes_rows(cursor, data):
+    cursor.executemany(
+        """
+        INSERT INTO votes (link, category, votes) 
+        VALUES (:link, :category, :votes)
+        ON CONFLICT DO UPDATE
+        SET votes = excluded.votes
+        """,
+        data,
+    )
+
+
 @click.command()
 @click.pass_context
 def enrich(ctx):
     """Navigate to previously scraped fragrance urls to get categorization data."""
-    config = ctx.obj.get("config")
-
-    with open(config["parfumo_import_results_path"], "r") as f:
-        collection = json.load(f)
+    collection = _get_collection()
 
     with start_driver() as driver:
-        fragrances_enriched = []
         # Let this loop run with the Chrome driver on screen, and wait until it finishes.
         for fragrance in tqdm(collection):
             driver.get(fragrance["link"])
@@ -52,19 +81,17 @@ def enrich(ctx):
             scent_audience = _get_chart_data(driver, 6, 1)
             scent_season = _get_chart_data(driver, 10, 3)
 
-            fragrances_enriched.append(
+            all_votes = [*scent_type, *scent_occasion, *scent_audience, *scent_season]
+            for item in all_votes:
+                item["link"] = fragrance["link"]
+                item["category"] = item["ct_name"]
+
+            _update_collection_row(
                 {
                     "name": name,
                     "brand": brand,
                     "image_src": image_src,
                     "link": fragrance["link"],
-                    "type": scent_type,
-                    "occasion": scent_occasion,
-                    "audience": scent_audience,
-                    "season": scent_season,
-                    "collection_group": fragrance["label"],
                 }
             )
-
-        with open(config["parfumo_enrich_results_path"], "w") as f:
-            json.dump(fragrances_enriched, f)
+            _update_votes_rows(all_votes)
