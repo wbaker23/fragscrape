73f894e0f7ddcc34a2caabae9fd41cb242a39835
Will Baker <wbaker@Wills-MBP.attlocal.net>
2024-06-09 22:56:41 -0500

    update graph_with_target_node.py

--- graph_with_target_node.py
+++ graph_with_target_node.py
@@ -5,15 +5,17 @@ import networkx as nx
 from fragscrape.parfumo.create_graph import MplColorHelper
 from fragscrape.parfumo.display_graph import visualize
 
+central_node = "https://www.parfumo.com/Perfumes/Louis_Vuitton/imagination"
+path_length = 1
+
 if __name__ == "__main__":
     with open("data/parfumo/collection/graph.json", "r") as f:
         graph = json.load(f)
     net = nx.node_link_graph(graph, multigraph=False)
 
-    central_node = "https://www.parfumo.com/Perfumes/Creed/Silver_Mountain_Water"
     node_path_lengths = sorted(
         [
-            (node, nx.shortest_path_length(net, central_node, node, "weight"))
+            (node, nx.shortest_path_length(net, central_node, node))
             for node in net.nodes()
         ],
         key=lambda x: x[1],
@@ -26,7 +28,13 @@ if __name__ == "__main__":
     nx.set_node_attributes(
         net,
         {
-            node: {"color": node_colors.get_rgb_str(weight)}
+            node: {
+                "color": (
+                    node_colors.get_rgb_str(weight)
+                    if weight == path_length
+                    else "rgb(255,255,255)"
+                )
+            }
             for node, weight in node_path_lengths
         },
     )
